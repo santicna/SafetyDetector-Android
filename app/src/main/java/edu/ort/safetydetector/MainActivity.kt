b0698eba09c03b6f46bc53522f648468a30ecc88
Santiago Cartagena <scartagena147@gmail.com>
2022-05-05 20:31:02 -0300

    Initial commit

---
+++ app/src/main/java/edu/ort/safetydetector/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.ort.safetydetector
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
